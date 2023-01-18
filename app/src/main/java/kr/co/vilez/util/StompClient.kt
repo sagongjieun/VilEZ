5b14be54f4597c4241050abb0897b8d63104e0f8
Sung GwangHyun <gch03944@gmail.com>
2023-01-18 17:58:02 +0900

    [Add] Kakao Map

---
+++ app/src/main/java/kr/co/vilez/util/StompClient.kt
@@ -0,0 +1,54 @@
+package kr.co.vilez.util
+
+import android.content.Context
+import android.util.Log
+import org.json.JSONObject
+import ua.naiksoftware.stomp.Stomp
+import ua.naiksoftware.stomp.dto.LifecycleEvent
+import ua.naiksoftware.stomp.dto.StompHeader
+
+class StompClient {
+    companion object {
+        private var instance: StompClient? = null
+
+        private lateinit var context: Context
+
+        fun getInstance(_context: Context): StompClient {
+            return instance ?: synchronized(this) {
+                instance ?: StompClient().also {
+                    context = _context
+                    instance = it
+                }
+            }
+        }
+        val url = "ws://211.216.215.157:8086/chat/websocket" // 소켓에 연결하는 엔드포인트가 /socket일때 다음과 같음
+        val stompClient =  Stomp.over(Stomp.ConnectionProvider.OKHTTP, url)
+
+        fun runStomp() {
+            val headerList = arrayListOf<StompHeader>()
+            stompClient.connect(headerList)
+
+            stompClient.lifecycle().subscribe { lifecycleEvent ->
+                when (lifecycleEvent.type) {
+                    LifecycleEvent.Type.OPENED -> {
+                        Log.i("OPEND", "!!")
+                    }
+                    LifecycleEvent.Type.CLOSED -> {
+                        Log.i("CLOSED", "!!")
+
+                    }
+                    LifecycleEvent.Type.ERROR -> {
+                        Log.i("ERROR", "!!")
+                        Log.e("CONNECT ERROR", lifecycleEvent.exception.toString())
+                    }
+                    else ->{
+                        Log.i("ELSE", lifecycleEvent.message)
+                    }
+                }
+            }
+
+
+        }
+    }
+
+}
