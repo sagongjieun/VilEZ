3a7f3dfc112444c485b71fee91ec15f9b643304d
jungeun-park-kr <je991025@gmail.com>
2023-01-19 14:45:25 +0900

    [Update] 로그인

--- app/src/main/java/kr/co/vilez/ui/user/ProfileFragment.kt
+++ app/src/main/java/kr/co/vilez/ui/user/ProfileFragment.kt
@@ -1,60 +1,58 @@
 package kr.co.vilez.ui.user
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kr.co.vilez.R
+import kr.co.vilez.data.model.User
+import kr.co.vilez.databinding.FragmentProfileBinding
+import kr.co.vilez.ui.IntroActivity
+import kr.co.vilez.ui.MainActivity
+import kr.co.vilez.util.ApplicationClass
+import retrofit2.awaitResponse
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+private const val TAG = "빌리지_ProfileFragment"
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+    private lateinit var binding:FragmentProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    fun login(view:View) {
+        Log.d(TAG, "login: adf")
+        CoroutineScope(Dispatchers.Main).launch {
+            val user = User("test", "12345")
+            val result = ApplicationClass.retrofitUserService.getLoginResult(user).awaitResponse().body()
+            if (result == null) { // 로그인 실패
+                Log.d(TAG, "login: 로그인 실패, result:$result")
+                binding.tvProfileUserInfo.text = "로그인 실패"
+            } else if(result.flag == "success") {  // 로그인 성공
+                Log.d(TAG, "로그인 성공, 받아온 user = ${result}")
+                binding.tvProfileUserInfo.text = result.data.toString()
+
             }
+        }
     }
+
+    fun logout(view:View) {
+
+    }
+
+
 }
