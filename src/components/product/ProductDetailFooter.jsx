e9ab30ed79b483ae36af0ba0184ed49d5d2cf0d3
사공지은 <gonii0821@naver.com>
2023-01-22 23:57:00 +0900

    [Add] 페이지 상단 이동 구현

--- src/components/product/ProductDetailFooter.jsx
+++ src/components/product/ProductDetailFooter.jsx
@@ -4,17 +4,30 @@ import { css } from "@emotion/react";
 import { HiChevronRight } from "react-icons/hi2";
 
 const ProductDetailFooter = () => {
+  function onClickReportArticle() {
+    /** 모달 띄우기 */
+    alert("정말 신고하시겠습니까?");
+  }
+  function onClickShareArticle() {
+    // 클립보드에 url 복사하기
+    alert("공유하시겠습니까?");
+  }
+
+  function onClickMoveToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
   return (
     <div css={menusWrapper}>
-      <div>
+      <div onClick={onClickReportArticle}>
         <span>이 게시물 신고하기</span>
         <HiChevronRight size="22" />
       </div>
-      <div>
+      <div onClick={onClickShareArticle}>
         <span>이 게시물 공유하기</span>
         <HiChevronRight size="22" />
       </div>
-      <div>
+      <div onClick={onClickMoveToTop}>
         <span>맨 위로 이동하기</span>
         <HiChevronRight size="22" />
       </div>
@@ -25,6 +38,7 @@ const ProductDetailFooter = () => {
 const menusWrapper = css`
   display: flex;
   flex-direction: column;
+  margin-top: 30px;
 
   & > div {
     width: 180px;
