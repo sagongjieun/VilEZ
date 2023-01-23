49b44e4585c55775be8244dccdf47330f96aa644
김종혁 <notify9637@naver.com>
2023-01-23 18:36:54 +0900

    [modify]서명 기능 구현

--- src/components/modal/CertifyVilez.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-/** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-
-function CertifyVilez() {
-  return <div></div>;
-}
-export default CertifyVilez;
