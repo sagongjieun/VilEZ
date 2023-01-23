a95d219fffa3c4327668077227be5598d986c53c
sagongjieun <gonii0821@naver.com>
2023-01-23 18:12:22 +0900

    [Add] 채팅 페이지 레이아웃 구현

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ProductRegist from "../components/product/ProductRegist";
 import ProductDetail from "../components/product/ProductDetail";
+import ProductChatting from "../components/product/ProductChatting";
 
 const Product = () => {
   return (
@@ -9,6 +10,7 @@ const Product = () => {
       <Routes>
         <Route path="/regist" element={<ProductRegist />} />
         <Route path="/detail" element={<ProductDetail />} />
+        <Route path="/chat" element={<ProductChatting />} />
       </Routes>
     </div>
   );
