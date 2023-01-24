2d4c1021d58114d00ce6ec418197bfcce0e1a1f0
jungeun-park-kr <je991025@gmail.com>
2023-01-24 14:59:38 +0900

    [Add] RecyclerView test

--- app/src/main/java/kr/co/vilez/ui/share/ShareActivity.java
+++ app/src/main/java/kr/co/vilez/ui/share/ShareActivity.java
@@ -1,5 +1,6 @@
 package kr.co.vilez.ui.share;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,7 +15,10 @@ import java.util.ArrayList;
 
 import kr.co.vilez.R;
 
+
 public class ShareActivity extends AppCompatActivity {
+    private static final String TAG = "빌리지_ShareActivity";
+
     private int i = 0;
     private ArrayList<ShareData> shareDatas;
     private ShareAdapter shareAdapter;
@@ -24,25 +28,36 @@ public class ShareActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_share);
+        setContentView(R.layout.activity_share);
 
-        recyclerView = findViewById(R.id.rv);
-        linearLayoutManager= new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(linearLayoutManager);
+        // 데이터 가져오기
+        shareDatas = new ArrayList<ShareData>();
+        shareDatas.add(new ShareData(R.drawable.img_default_profile, "test1"));
+        shareDatas.add(new ShareData(R.drawable.img_default_profile, "test2"));
+        shareDatas.add(new ShareData(R.drawable.img_default_profile, "test3"));
 
-
-        shareDatas = new ArrayList<>();
+        // 어댑터 생성
         shareAdapter = new ShareAdapter(shareDatas);
 
+        // 리사이클러뷰에 어댑터 등록
+        recyclerView = findViewById(R.id.rv);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(shareAdapter);
+
+        Log.d(TAG, "onCreate: asdf ShareActivity ");
 
         Button btn_add = (Button) findViewById(R.id.btn_add);
         btn_add.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("NotifyDataSetChanged")
             @Override
             public void onClick(View view) {
+                Log.d(TAG, "onClick: ");
                 ShareData shareData = new ShareData(R.drawable.img_default_profile, "test"+i);
                 shareDatas.add(shareData);
                 shareAdapter.notifyDataSetChanged();
             }
         });
     }
+
+
 }
