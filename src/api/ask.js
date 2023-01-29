77b784918e5082cb93d02a32769fd6dbccb40c0c
사공지은 <gonii0821@naver.com>
2023-01-29 21:40:44 +0900

    [Add] api 요청 구현

--- src/api/ask.js
+++ src/api/ask.js
@@ -1,6 +1,6 @@
 import { jsonInstance, formdataInstance } from "./instance";
 
-const jsonAxios = jsonInstance(); //eslint-disable-line no-unused-vars
+const jsonAxios = jsonInstance();
 const formdataAxios = formdataInstance();
 
 // GET
@@ -9,7 +9,30 @@ async function getAskArticleList() {
   try {
     const { data } = await jsonAxios.get(`/askboard`);
 
-    return data;
+    if (data.flag === "success") return data.data;
+    else console.log("요청 글 목록을 불러오는 데 실패하였습니다. 😥");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getAskArticleDetailByBoardId(boardId) {
+  try {
+    const { data } = await jsonAxios.get(`/askboard/detail/${boardId}`);
+
+    if (data.flag === "success") return data.data;
+    else console.log("일치하는 게시글이 없습니다. 😥");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getMyAskArticle(userId) {
+  try {
+    const { data } = await jsonAxios.get(`/askboard/my/${userId}`);
+
+    if (data.flag === "success") return data.data;
+    else console.log("일치하는 회원이 없습니다. 😥");
   } catch (error) {
     console.log(error);
   }
@@ -28,4 +51,37 @@ async function postAskArticle(formData) {
   }
 }
 
-export { getAskArticleList, postAskArticle };
+// DELETE
+
+async function deleteAskArticleByBoardId(boardId) {
+  try {
+    const { data } = await jsonAxios.delete(`/askboard/${boardId}`);
+
+    if (data.flag === "success") alert("요청글 삭제에 성공하였습니다. 😀");
+    else alert("요청 글 삭제에 실패하였습니다 😥");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// PUT
+
+async function putAskArticle(formData) {
+  try {
+    const { data } = await formdataAxios.put(`/askboard`, formData);
+
+    if (data.flag === "success") alert("수정되었습니다 😀");
+    else alert("요청 글 수정에 실패하였습니다 😥");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export {
+  getAskArticleList,
+  getAskArticleDetailByBoardId,
+  getMyAskArticle,
+  postAskArticle,
+  deleteAskArticleByBoardId,
+  putAskArticle,
+};
