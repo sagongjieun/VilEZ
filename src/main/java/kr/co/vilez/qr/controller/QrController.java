e632dbdf3fe29a5c21977ce0e832dec6efdddf75
SeonyongPark <tony3337@naver.com>
2023-02-02 15:48:32 +0900

    [Modify] modify users

--- src/main/java/kr/co/vilez/qr/controller/QrController.java
+++ src/main/java/kr/co/vilez/qr/controller/QrController.java
@@ -18,6 +18,8 @@ public class QrController {
 
     final QrService qrService;
     @PostMapping()
+    @ApiOperation(value = "QR 코드를 생성하는 API",
+            notes = "userId를 보내주면 해당하는 qr코드를 생성해준다.")
     public ResponseEntity<?> createQr(int userId) {
         HttpVO httpVO = new HttpVO();
         ArrayList<Object> data = new ArrayList<>();
@@ -26,6 +28,24 @@ public class QrController {
             data.add(qrService.createQR(userId));
             httpVO.setFlag("success");
             httpVO.setData(data);
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+        return new ResponseEntity<HttpVO>(httpVO, HttpStatus.OK);
+    }
+
+    @DeleteMapping()
+    @ApiOperation(value = "QR 코드를 삭제하는 API",
+            notes = "해당 이미지의 url을 보내주면 해당하는 qr코드를 삭제해준다." +
+                    "\n\t https://kr.object.ncloudstorage.com/vilez/qr/1/5446758498600 이미지 url이 이렇다면" +
+                    "\n\t qr/1/5446758498600 이것만 보내줘요")
+    public ResponseEntity<?> deleteQr(String imgUrl){
+        HttpVO httpVO = new HttpVO();
+
+        try{
+            qrService.deleteQr(imgUrl);
+            httpVO.setFlag("success");
         } catch(Exception e){
             e.printStackTrace();
         }
--- src/main/java/kr/co/vilez/qr/model/service/QrService.java
+++ src/main/java/kr/co/vilez/qr/model/service/QrService.java
@@ -5,5 +5,6 @@ import kr.co.vilez.qr.model.dto.QrCodeDto;
 import java.util.Map;
 
 public interface QrService {
+    void deleteQr(String url) throws Exception;
     Map<String, String> createQR(int userId) throws Exception;
 }
