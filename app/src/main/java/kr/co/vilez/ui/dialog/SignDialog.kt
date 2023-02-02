0ab6402ed107e8ecd8cc8370a59842b91a288827
jungeun-park-kr <je991025@gmail.com>
2023-02-03 05:44:14 +0900

    [Add] 채팅 공유기간 설정 데이트 피커 추가

---
+++ app/src/main/java/kr/co/vilez/ui/dialog/SignDialog.kt
@@ -0,0 +1,12 @@
+package kr.co.vilez.ui.dialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.vilez.R
+
+class SignDialog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_dialog)
+    }
+}
