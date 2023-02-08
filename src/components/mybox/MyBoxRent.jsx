94cfd98203f84209e49577ba27304939c9f3c844
unknown <chopinoff@gmail.com>
2023-02-08 11:01:25 +0900

    [Modify] 캘린더 및 마이박스 (ing)

--- src/components/mybox/MyBoxRent.jsx
+++ src/components/mybox/MyBoxRent.jsx
@@ -1,45 +1,101 @@
 import React, { useState, useEffect } from "react";
 /** @jsxImportSource @emotion/react */
-import { css } from "@emotion/react";
-// import { Routes, Route } from "react-router-dom";
+import { css, keyframes } from "@emotion/react";
+import { getUserShare, getUserAsk } from "../../api/profile";
+import ProfileCardView from "./MyBoxCardView";
 
-const MyBoxRent = () => {
-  // const userId = localStorage.getItem("id");
-  const [rentNow, setRentNow] = useState(true);
-
+// const id = localStorage.getItem("id");
+const MyBoxRent = (props) => {
+  const userId = localStorage.getItem("id");
+  const [myBoard, setMyBoard] = useState([]);
+  const [myBeingRentedBoard, setMyBeingRentedBoard] = useState([]);
+  const [myToBeRentedBoard, setMyToBeRentedBoard] = useState([]);
   useEffect(() => {
-    setRentNow(true);
-    console.log(rentNow);
+    getUserShare(userId).then((response) => {
+      setMyBoard(response);
+      setMyBeingRentedBoard(response);
+      console.log("rentPage", response);
+    });
+    getUserAsk(userId).then((response) => {
+      setMyToBeRentedBoard(response);
+    });
   }, []);
-  return <div css={mainWrapper}></div>;
+  useEffect(() => {
+    props.setRentPages(1);
+    if (props.myRentType === 1) {
+      setMyBoard(myBeingRentedBoard);
+    } else {
+      setMyBoard(myToBeRentedBoard);
+    }
+  }, [props.myRentType]);
+  useEffect(() => {
+    props.setRentDefaultPages(parseInt((myBoard?.length - 1) / 3) + 1);
+    console.log(myBoard);
+  }, [myBoard]);
+  return (
+    <div css={cardWrapper(props.rentPages)}>
+      {myBoard?.length > 0 ? (
+        myBoard.map((share, idx) => (
+          <div key={idx}>
+            <ProfileCardView
+              title={share.title}
+              endDay={share.endDay}
+              startDay={share.startDay}
+              date={share.date}
+              thumbnail={share.list[0]?.path}
+              boardType={1}
+              boardId={share.id}
+            />
+          </div>
+        ))
+      ) : (
+        <div css={noCards}>작성한 글이 없습니다.</div>
+      )}
+    </div>
+  );
 };
-const mainWrapper = css``;
-// const basicButton = css`
-//   cursor: pointer;
-//   height: 35px;
-//   background-color: #66dd9c;
-//   width: 100%;
-//   font-size: 14px;
-//   border: 1px solid #66dd9c;
-//   color: #fff;
-// `;
-// const borderedButton = css`
-//   cursor: pointer;
-//   height: 35px;
-//   background-color: #fff;
-//   width: 100%;
-//   font-size: 14px;
-//   border: 1px solid #66dd9c;
-//   color: #66dd9c;
-//   :hover {
-//     background-color: #acf0cb32;
-//   }
-// `;
-// const squareButton = css`
-//   border-radius: 5px;
-// `;
-// const roundButton = css`
-//   border-radius: 20px;
-// `;
+
+const appear = keyframes`
+  0% {
+    display: none;
+    opacity: 0;
+  }
+  100% {
+    display: block;
+    opacity: 1;
+  }
+`;
+const cardWrapper = (pages) => {
+  const cards = pages * 3;
+  return css`
+    position: relative;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    row-gap: 20px;
+    column-gap: 20px;
+    height: calc(6px + ${pages}* 274px);
+    transition: all 0.5s;
+    & > div {
+      display: none;
+      opacity: 1;
+      min-width: 300px;
+      transition: all 0.3s;
+    }
+    & > div:nth-of-type(-n + ${cards}) {
+      display: block;
+      animation-name: ${appear};
+      animation-duration: 0.3s;
+      transition: all 0.3s;
+    }
+  `;
+};
+
+const noCards = css`
+  position: absolute;
+  line-height: 200px;
+  height: 200px;
+  width: 100%;
+  text-align: center;
+`;
 
 export default MyBoxRent;
