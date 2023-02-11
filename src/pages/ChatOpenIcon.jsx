515ed567a74e2cef77d172daa78c3fc6dcf37cb9
성광현 <gch03944@gmail.com>
2023-02-12 01:00:51 +0900

    [Modify] 채팅 버그 수정 및 로그아웃시 새로고침

--- src/pages/ChatOpenIcon.jsx
+++ src/pages/ChatOpenIcon.jsx
@@ -3,13 +3,13 @@ import { BsChatSquare } from "react-icons/bs";
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import ChattingModal from "../components/modal/ChattingModal";
-import { modalOpenState, enterChatRoomState, chatListState, boardState, isLoginState } from "../recoil/atom";
-import { useRecoilState, useSetRecoilState, useRecoilValue } from "recoil";
+import { modalOpenState, enterChatRoomState, chatListState, boardState } from "../recoil/atom";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import SockJS from "sockjs-client";
 import { Stomp } from "@stomp/stompjs";
 
 const client = Stomp.over(function () {
-  return new SockJS(`${process.env.REACT_APP_API_BASE_URL}/chat`); // STOMP 서버가 구현돼있는 url
+  return new SockJS(`${process.env.REACT_APP_API_BASE_URL}/chat`);
 });
 
 function ChatOpenIcon() {
@@ -18,30 +18,16 @@ function ChatOpenIcon() {
   const [isNewMessage, setIsNewMessage] = useState(false);
   const [enterChatRoom, setEnterChatRoom] = useRecoilState(enterChatRoomState);
   const setChatList = useSetRecoilState(chatListState);
-  // const [stomp, setStomp] = useRecoilState(stompState);
   const [isSocketConnected, setIsSocketConnected] = useState(false);
   const [boardFlag, setBoardFlag] = useRecoilState(boardState);
-  //useSetRecoilState
   const onClickOpenChat = () => {
-    // if (!modalOpen) setIsNewMessage(false); // 새로운메시지를 확인했다면 알림 지우기
     setModalOpen(!modalOpen);
   };
-  const isLogin = useRecoilValue(isLoginState);
 
   useEffect(() => {
     if (loginUserId) {
-      // client = Stomp.over(function () {
-      //   return new SockJS(`${process.env.REACT_APP_API_BASE_URL}/chat`); // STOMP 서버가 구현돼있는 url
-      // }); // 웹소켓 클라이언트 생성
-
-      // 웹소켓과 연결됐을 때 동작하는 콜백함수들
       client.connect({}, () => {
         client.subscribe(`/sendlist/${loginUserId}`, () => {
-          // data = JSON.parse(data.body);
-          // 상대방이 메시지 보낼 때만 새로운 메시지 알림
-          // if (data.fromUserId != loginUserId) {
-          //   setIsNewMessage(true);
-          // }
           const payload = {
             userId: loginUserId,
           };
@@ -74,13 +60,11 @@ function ChatOpenIcon() {
 
   useEffect(() => {
     if (enterChatRoom && loginUserId && isSocketConnected) {
-      // 해당 방으로 들어갔다는 소켓 send
       const data = {
         roomId: enterChatRoom,
         userId: loginUserId,
       };
 
-      console.log("채팅 아이콘으로 들어갈 때 뜨는 오류 : ", data, client);
       client.send("/room_enter", {}, JSON.stringify(data));
       setEnterChatRoom(null);
     }
@@ -101,12 +85,10 @@ function ChatOpenIcon() {
 
   return (
     <>
-      {isLogin && (
-        <div css={IconBox} onClick={onClickOpenChat}>
-          <BsChatSquare size="25" />
-          {isNewMessage ? <div css={newMessageAlarm}></div> : <></>}
-        </div>
-      )}
+      <div css={IconBox} onClick={onClickOpenChat}>
+        <BsChatSquare size="25" />
+        {isNewMessage ? <div css={newMessageAlarm}></div> : <></>}
+      </div>
       {modalOpen ? <ChattingModal /> : null}
     </>
   );
