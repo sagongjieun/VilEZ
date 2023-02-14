7e4942faa91bcda43da8ced2261c0fb58f621300
Sung GwangHyun <gch03944@gmail.com>
2023-02-14 17:38:33 +0900

    [Fix] 지도

--- src/components/StompRealTime.jsx
+++ src/components/StompRealTime.jsx
@@ -116,9 +116,7 @@ const StompRealTime = ({
 
   // Map에서 받은 데이터로 서버에 전송
   function receiveLocation(location, lat, lng, zoomLevel, isMarker) {
-    console.log("확인1");
     if (lat && lng && isMarker) {
-      console.log("확인2");
       searchDetailAddrFromCoords(lat, lng, function (result, status) {
         if (status === kakao.maps.services.Status.OK) {
           setHopeLocation(result[0].address.address_name);
@@ -127,9 +125,8 @@ const StompRealTime = ({
     } else {
       setHopeLocation(location);
     }
-    console.log("확인3");
 
-    if (lat && lng && zoomLevel && location !== "마우스 우클릭으로 장소를 선택해주시면 돼요") {
+    if (lat && lng && zoomLevel) {
       const sendMapData = {
         roomId: chatRoomId,
         toUserId: otherUserId,
@@ -138,10 +135,7 @@ const StompRealTime = ({
         zoomLevel: zoomLevel,
         isMarker: isMarker,
       };
-
-      console.log("확인4");
       client.send("/recvmap", {}, JSON.stringify(sendMapData));
-      console.log("확인5");
     }
   }
 
