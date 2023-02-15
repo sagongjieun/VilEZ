bd1b508d44420309403c823ecf64a2aec3c73dc6
SeonyongPark <tony3337@naver.com>
2023-02-15 16:26:22 +0900

    [Modify] modify

--- src/main/java/kr/co/vilez/jwt/JwtProviderImpl.java
+++ src/main/java/kr/co/vilez/jwt/JwtProviderImpl.java
@@ -23,8 +23,8 @@ public class JwtProviderImpl implements JwtProvider {
         return instacne;
     }
 
-    private long tokenValidTime = Duration.ofMinutes(5).toMillis();
-    private long refreshTokenValidTime = Duration.ofMinutes(10).toMillis();
+    private long tokenValidTime = Duration.ofMinutes(2).toMillis();
+    private long refreshTokenValidTime = Duration.ofDays(90).toMillis();
 
     @PostConstruct
     protected void init() {
